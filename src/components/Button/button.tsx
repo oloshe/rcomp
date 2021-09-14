f00f038105bc5ae725dcf3a42b92c672033d25ee
oloshe <jacob.j@qq.com>
2021-09-14 20:37:18 +0800

    feat: two mode of button

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export interface ButtonProps {
+	mode?: 'normal' | 'calm' | 'crazy' | 'insame'
 	disable?: boolean
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
-	return <div className="rc__btn">{props.children}</div>
+	let {
+		mode = 'normal',
+		disable = false
+	} = props
+
+	const [ani, setAni] = useState(false)
+
+	const onAniEnd = () => {
+		setAni(false)
+	}
+
+	const onClick = () => {
+		if (mode === 'calm') {
+			setAni(true)
+		}
+	}
+
+	return (
+		<div
+		className={`rc__btn${disable ? ' disable' : ''} ${mode} pr ani`}
+		onAnimationEnd={onAniEnd}
+		onClick={onClick}
+		data-ani={ani}
+		>
+			{props.children}
+		</div>
+	)
 }
 export default Button
